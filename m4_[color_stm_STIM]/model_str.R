bed58c89ac91dca7b7f681e4ea803e0d80631a8b
czawora <christopher.zawora@gmail.com>
2019-11-01 12:28:46 -0400

    last updates from NIH

--- m4_[color_stm_STIM]/model_str.R
+++ m4_[color_stm_STIM]/model_str.R
@@ -58,28 +58,58 @@ sample_size_notes_str <- "
 correct_check_notes_str <- ""
 
 vm_mean_prior_notes_str <- "
+Extreme mean effects are far more contained.
 "
 vm_pred_notes_str <- "
+Predictive distribution of subject are more contained pre/post and group-level SD is better contained.
+
+Still a lot probability on extreme max values, but overall these priors seem much more resonable.
 "
 
 pMem_mean_prior_notes_str <- "
+No changes to this.
 "
 
 pMem_pred_notes_str <- "
+Though, the prior on group-level variance was narrowed and that makes these subject pred dists look much better. Less extreme values.
 "
 
 obs_pred_notes_str <- "
+Clearly no predicted effect. Post condition shows the same, expected increase in subject pred dist variance.
 "
 
 ##############################################
 # model fit
 
-fit_qc_notes_str <- ""
-posterior_notes_str <- ""
-posterior_pred_notes_str <- ""
+fit_qc_notes_str <- "All looks good."
+posterior_notes_str <- "Seems good in that there is consistent reduction in uncertainty from prior to posterior. 
+
+I should come up with a better plot to show the posterior estimates of group-level variance.
+"
+posterior_pred_notes_str <- "Both the reduction in uncertainty and the hint of an effect are visible in the marginal subject plot and the average subject plot."
 
 ##############################################
 # loo
 
 loo_notes_str <- "
+Same ordering of waic differences as in m6.
+
+
+WAIC diffs were not very large and had non-neglible SE. Also inconclusive.
+
+waic diff ordering:
+
+-fit_DpMem_null      
+
+-fit_DcircSD_DpMem_null 
+
+-fit_full               
+
+-fit_DcircSD_null
+
+fit_DpMem_null predicted best since it omits the difference parameter on pMem, for which there seems little evidence of an effect.
+
+fit_DcircSD_null fit worst since it omits the parameter that seems to be actually showing a non-zero value. 
+
+fit_full and fit_DcircSD_DpMem_null are in the middle.
 "
--- m4_[color_stm_STIM]/render.R
+++ m4_[color_stm_STIM]/render.R
@@ -8,6 +8,7 @@ model_dir <- "/Users/zaworaca/Desktop/Desktop/zane/color_twoCond_diff/m4_[color_
 # load math model str
 source(glue("{model_dir}/model_str.R"))
 
+
 ###########################################
 # knit math_model markdown file
 render(glue("{common_dir}/math_model.Rmd"), 
@@ -16,6 +17,7 @@ render(glue("{common_dir}/math_model.Rmd"),
          formula_str = math_model_str
        ))
 
+
 ###########################################
 # knit prior_predictive_check markdown file
 prior_check_param_list <- list(
@@ -23,16 +25,22 @@ prior_check_param_list <- list(
   formula_str = math_model_str,
   top_notes_str = prior_check_top_notes_str,
   sample_size_sim_str = sample_size_notes_str,
+  correct_check_notes_str = correct_check_notes_str,
+  vm_mean_prior_notes_str = vm_mean_prior_notes_str, 
+  vm_pred_notes_str = vm_pred_notes_str,
+  pMem_mean_prior_notes_str = pMem_mean_prior_notes_str, 
+  pMem_pred_notes_str = pMem_pred_notes_str, 
+  obs_pred_notes_str = obs_pred_notes_str,
   
   model_dir_str = model_dir,
   common_dir_str = common_dir,
   save_dir_str = glue("{model_dir}/prior_predictive_check") 
-  )
+)
 
 render(glue("{common_dir}/prior_predictive_check.Rmd"),
-      output_dir = glue("{model_dir}/prior_predictive_check"),
-      params = prior_check_param_list)
-      
+       output_dir = glue("{model_dir}/prior_predictive_check"),
+       params = prior_check_param_list)
+
 
 ###########################################
 # knit sim model fit markdown file
@@ -53,6 +61,10 @@ render(glue("{common_dir}/sim_fit.Rmd"),
 # knit model_fitting markdown file
 model_fitting_param_list <- list(
   
+  fit_qc_notes_str = fit_qc_notes_str,
+  posterior_notes_str = posterior_notes_str,
+  posterior_pred_notes_str = posterior_pred_notes_str,
+  
   model_dir_str = model_dir,
   common_dir_str = common_dir,
   save_dir_str = glue("{model_dir}/model_fitting_actual") 
@@ -63,9 +75,12 @@ render(glue("{common_dir}/model_fitting.Rmd"),
        params = model_fitting_param_list)
 
 
+
 ###########################################
 # knit loo markdown file
 loo_param_list <- list(
+  
+  loo_notes_str = loo_notes_str,
   
   model_dir_str = model_dir,
   common_dir_str = common_dir,
